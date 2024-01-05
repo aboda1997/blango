81d3ea60a176cb251f453df5cf62b94bffd9b74d
aboda1997 <abdelrahmanalgendy97@gmail.com>
2024-01-05 19:37:54 +0200

    Finish generic relationships

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.conf import settings
 
+from django.contrib.contenttypes.fields import GenericForeignKey , GenericRelation 
+from django.contrib.contenttypes.models import ContentType 
+
+
+
 # Create your models here.
 
 
@@ -9,6 +14,20 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.value
+
+
+class Comment(models.Model): 
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE)
+    content_type  = models.ForeignKey(ContentType , on_delete= models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type' , 'object_id') 
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self): 
+        return self.content 
+
 
 
 class Post(models.Model):
@@ -22,6 +41,9 @@ class Post(models.Model):
     summary = models.TextField(max_length=500)
     content = models.TextField()
     tags = models.ManyToManyField(Tag, related_name="posts")
+    comments = GenericRelation(Comment)
 
     def __str__(self):
         return self.title
+
+
